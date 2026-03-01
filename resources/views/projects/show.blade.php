b0e9866a9668f787825be9a28a9554e1e0d8b27b
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2026-03-01 14:07:33 +0700

    add a top tab ( menu bar )

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,7 +4,20 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
 </head>
-<body class="bg-blue-200 p-4">
+<body class="bg-blue-200">
+
+<div class="bg-blue-950 text-white 100">
+
+    <ul class="flex space-x-4 p-4">
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('projects.index') }}">Projects</a></li>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Settings</a></li>
+    </ul>   
+
+</div>
+
+<div class="p-4">
 
 <h1 class="text-3xl font-bold">{{ $project->title }}</h1>
 <p><strong>Penname: {{ $project->penname }}</strong></p>
@@ -19,12 +32,9 @@
     <ul class="list-disc list-inside">
     @foreach($project->characters as $character)
         <li>
-                <strong>Name:</strong> {{ $character->name }}
-
-                <strong>Role:</strong> {{ $character->role }}
-
-            <a href="#">Character's detail: url of that charcater</a>
-
+            <p><strong>Name:</strong> {{ $character->name }}</p>
+            <p><strong>Role:</strong> {{ $character->role }}</p>
+            <p><a href="#">Character's detail: url of that character</a></p>
             <p><a href="#">Add new character: go to character page</a></p>
         </li>
     @endforeach
@@ -36,5 +46,6 @@
 
 
 <a href="{{ route('projects.index') }}">Back to list of projects</a>
+</div>
 </body>
 </html>
