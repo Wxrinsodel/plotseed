13e701d7593693af3de31e7d847797e961da7631
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2026-03-01 21:16:02 +0700

    Remove vite and replace by tailwindcss to login and create acc ( AUTH PAGE)

--- resources/views/components/site-navigation.blade.php
+++ resources/views/components/site-navigation.blade.php
@@ -1,10 +1,38 @@
 <nav class="bg-blue-950 text-white 100">
+ 
+    <div class="max-w-7xl mx-auto flex justify-between items-center h-16 px-4">
+        <ul class="flex space-x-4 p-4 "> 
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('projects.index') }}">Projects</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Settings</a></li>
+        </ul>   
 
-    <ul class="flex space-x-4 p-4 ">
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('projects.index') }}">Projects</a></li>
-        <li><a href="#">Characters</a></li>
-        <li><a href="#">Settings</a></li>
-    </ul>   
+            @if (Route::has('login'))
+                <div class="flex  items-center justify-end gap-4">
+                    @auth
+                        <a
+                            href="{{ url('/dashboard') }}"
+                            class="">
+                            Dashboard
+                        </a>
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="">
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="">
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </nav>
+            @endif
+        </header>
 
 </nav>
