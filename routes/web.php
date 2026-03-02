4a3595c9fd20841e78dc4e1f8d1a0828f71a2a95
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2026-03-02 11:53:16 +0700

    redirect the route to let it know a way to go when user clink on the buttom or url

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,15 @@
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
+
 /*
  * Public routes
  */
 
 Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('home');
 
-
-Route::get('projects', [\App\Http\Controllers\ProjectController::class, 'index'])->name('projects.index');
-Route::get('projects/{project}', [\App\Http\Controllers\ProjectController::class, 'show'])->name('projects.show');
 Route::resource('projects', ProjectController::class);
+Route::get('projects/{project}', [\App\Http\Controllers\ProjectController::class, 'show'])->name('projects.show');
 
 /*
  * User zone routes
