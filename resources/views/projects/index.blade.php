298e73c196c8a40c563161ba174caaa2959b077c
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2026-03-02 11:54:51 +0700

    add a button and loop to show all projects

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,11 +1,17 @@
 <x-site-layout title="List of projects">
+<div class="mb-8 flex justify-between items-center">
+        <h1 class="text-3xl font-bold">List of Projects</h1>
+        <a href="{{ route('projects.create') }}" class="bg-green-500 text-white px-6 py-2 rounded-full font-bold hover:bg-green-600">
+            + Add New Project
+        </a>
+    </div>
 
-
-    <h1 class="inline text-xl font-semibold text-black-800">List of Projects</h1>
-    <ul class="list-disc list-inside">
-        @foreach($projects as $project)
-            <li> <a href="{{ route('projects.show', $project->id) }}">{{ $project->title }}</a></li>
-        @endforeach
-    </ul>
+    @foreach($projects as $project)
+        <div class="p-6 border-b hover:bg-gray-50 transition">
+             <h2 class="text-xl font-bold text-blue-800">{{ $project->title }}</h2>
+             <p class="text-gray-600">penname: {{ $project->penname }} | Genre: {{ $project->genre }}</p>
+             <a href="{{ route('projects.show', $project->id) }}" class="mt-2 inline-block text-blue-500 font-semibold underline">Manage Project</a>
+        </div>
+    @endforeach
 
 </x-site-layout>
