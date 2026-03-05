7f91440388c10f25e316ef024a91b55a70c9b88d
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2026-03-05 21:15:49 +0700

    Add foreign keys to my migrations

--- database/migrations/2026_02_28_101841_create_projects_table.php
+++ database/migrations/2026_02_28_101841_create_projects_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id(); 
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+
             $table->string('penname');
             $table->string('title');
             $table->text('outline')->nullable();
--- database/migrations/2026_03_01_054744_create_characters_table.php
+++ database/migrations/2026_03_01_054744_create_characters_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('characters', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+
             $table->string('name');
             $table->string('role') ->nullable();
             $table->text('identity')->nullable();
